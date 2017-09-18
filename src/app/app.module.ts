f2c671d6677c775ceda13ab60167cde6f92de18d
RICO MAI <ricomai@ricos-mac.local>
2017-09-18 11:49:22 -0400

    add animation for arrow

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+
 
 import { AppComponent } from './app.component';
 import { HomeModule } from './home/home.module';
@@ -11,6 +13,7 @@ import { HomeModule } from './home/home.module';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     HomeModule
   ],
   providers: [],
--- src/app/home/home-content-component/home-content-component.component.ts
+++ src/app/home/home-content-component/home-content-component.component.ts
@@ -1,18 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,
+         OnInit,
+         animate,
+         keyframes,
+         trigger,
+         state,
+         style,
+         transition} from '@angular/core';
 
 @Component({
   selector: 'app-home-content-component',
   templateUrl: './home-content-component.component.html',
-  styleUrls: ['./home-content-component.component.css']
+  styleUrls: ['./home-content-component.component.css'],
+  animations: [
+    trigger("flyLeftOut",[
+      state('off',style({transform:'translateX(0)'})),
+      state('on',style({transform:'translateX(-100%)'})),
+      transition("off=>on",animate(2000,keyframes([
+        style({opacity:1,transform:'translateX(0)',offset:0}),
+        style({opacity:1,transform:'translateX(30px)',offset:0.4}),
+        style({opacity:0,transform:'translate(-100%)',offset:1})
+      ])))
+    ]),
+    trigger("flyRightOut",[
+      state('off',style({transform:'translateX(0)'})),
+      state('on',style({transform:'translateX(100%)'})),
+      transition("off=>on",animate(2000,keyframes([
+        style({opacity:1,transform:'translateX(0)',offset:0}),
+        style({opacity:1,transform:'translateX(-30px)',offset:0.4}),
+        style({opacity:0,transform:'translate(100%)',offset:1})
+      ])))
+    ])
+  ]
+
 })
 export class HomeContentComponentComponent implements OnInit {
 
   dev_img_url = './../../../assets/imgs/dev_img.jpg';
   sports_img_url = './../../../assets/imgs/roger.jpg';
+  leftState:string = 'off';
+  rightState:string = 'off';
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  toDev(){
+    this.leftState = (this.leftState==='off') ? "on" :'off';
+  };
+
+  toHobbies(){
+    this.rightState = (this.rightState ==='off') ? "on" : "off";
+  };
 }
