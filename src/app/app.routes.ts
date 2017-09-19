a893015ed28fa0edafefe97437f632a1733280db
RICO MAI <ricomai@ricos-mac.local>
2017-09-19 15:09:48 -0400

    first version

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,7 +6,7 @@ import { HobbiesComponent } from "./hobbies/hobbies.component";
 
 export const routes:Routes =[
   {path:"home",component:HomeComponent},
-  // {path:'',redirectTo:'/home',pathMatch:'full'},
+  {path:'',redirectTo:'/home',pathMatch:'full'},
   {path:'hobbies',component:HobbiesComponent},
   {path:'dev',component:DevComponent}
 
