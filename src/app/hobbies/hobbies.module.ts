e4d9371d8466597bbca308163d16ddc7e3f893e3
RICO MAI <ricomai@ricos-mac.home>
2017-09-18 20:02:05 -0400

    add hobbies module

---
+++ src/app/hobbies/hobbies.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HobbiesComponent } from '../src/app/hobbies/hobbies.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [HobbiesComponent]
+})
+export class HobbiesModule { }
